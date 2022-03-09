3d97a8635e1a70fd05383be0fbf15579e8b7b6c2
Gulkovva19 <gulkova.sofya@yandex.ru>
2022-03-09 20:10:26 +0300

    homework

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 function onSearch() {
     let gif = document.getElementById("gifsearch").value;
 
-    fetch('https://api.giphy.com/v1/gifs/search?api_key=Qvf9S52TwRjgZhRgCtYHB9Xm1bMAjTj4&q=' + gif + '&limit=1&offset=0&rating=g&lang=e')
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=Qvf9S52TwRjgZhRgCtYHB9Xm1bMAjTj4&q=' + gif + '&limit=5&offset=0&rating=g&lang=e')
         .then(response => response.json())
         .then(gifka => {
-            console.log(gifka);
+            const div1 = document.getElementById('div1');
+            gifka.data.forEach(element => {
+                const div2 = document.createElement('a');
+                div2.href = element.url;
+                div2.appendChild(document.createTextNode('Посмотреть'));
+                console.log(div2);
+                div1.appendChild(div2);
+                console.log(div1);
+            })
         })
         .catch(error => console.log(error));
 }
